015c91330d8e4c75f63c0b5f0800392f7ed8bc92
tuanpembual <andro.medh4@gmail.com>
2014-10-22 14:34:48 +0700

    initial push

---
+++ crawl.py
@@ -0,0 +1,81 @@
+import twitter
+import json
+
+# XXX: Go to http://dev.twitter.com/apps/new to create an app and get values
+# for these credentials, which you'll need to provide in place of these
+# empty string values that are defined as placeholders.
+# See https://dev.twitter.com/docs/auth/oauth for more information 
+# on Twitter's OAuth implementation.
+
+CONSUMER_KEY = 'C5lJE7T2QoQ4kCLmCYRj61SaD'
+CONSUMER_SECRET = 'DULtzlHgGfnkO0GNtIpyYBvqW1M4hEfHuWrZ0m5JKnmedj2ljy'
+OAUTH_TOKEN = '79529075-6fLXfXus6xyJN42KJG7vm2Apyqq5xQwanKM68vnMv'
+OAUTH_TOKEN_SECRET = 'dSteIYhb9FGPoeyX3gz6b5zVJT4ny0nlNNhQ31evGn0sr'
+
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                           CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_api = twitter.Twitter(auth=auth)
+
+# Nothing to see by displaying twitter_api except that it's now a
+# defined variable
+
+# print twitter_api
+
+# The Yahoo! Where On Earth ID for the entire world is 1.
+# See https://dev.twitter.com/docs/api/1.1/get/trends/place and
+# http://developer.yahoo.com/geo/geoplanet/
+
+WORLD_WOE_ID = 1
+US_WOE_ID = 23424977
+
+# Prefix ID with the underscore for query string parameterization.
+# Without the underscore, the twitter package appends the ID value
+# to the URL itself as a special case keyword argument.
+
+world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
+us_trends = twitter_api.trends.place(_id=US_WOE_ID)
+
+#print world_trends
+#print
+#print us_trends
+
+#print json.dumps(world_trends, indent=1)
+#print
+#print json.dumps(us_trends, indent=1)
+
+# XXX: Set this variable to a trending topic, 
+# or anything else for that matter. The example query below
+# was a trending topic when this content was being developed
+# and is used throughout the remainder of this chapter.
+
+q = '#RabuSoto' 
+# key pencarian
+
+count = 10
+
+# See https://dev.twitter.com/docs/api/1.1/get/search/tweets
+
+search_results = twitter_api.search.tweets(q=q, count=count)
+
+statuses = search_results['statuses']
+
+
+# Iterate through 5 more batches of results by following the cursor
+
+for _ in range(5):
+    #print "Length of statuses", len(statuses)
+    try:
+        next_results = search_results['search_metadata']['next_results']
+    except KeyError, e: # No more results when next_results doesn't exist
+        break
+        
+    # Create a dictionary from next_results, which has the following form:
+    # ?max_id=313519052523986943&q=NCAA&include_entities=1
+    kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
+    
+    search_results = twitter_api.search.tweets(**kwargs)
+    statuses += search_results['statuses']
+
+# Show one sample search result by slicing the list...
+print json.dumps(statuses[0], indent=1)
