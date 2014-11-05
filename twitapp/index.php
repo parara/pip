a18a804162623e302991b6ca9670ca1dc947b060
tuanpembual <andro.medh4@gmail.com>
2014-11-05 08:14:53 +0700

    add table langganan

--- twitapp/index.php
+++ twitapp/index.php
@@ -55,7 +55,7 @@ include('config.php');
       <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
       <table class="table table-hover sortable">
       <?php
-      $tampil = mysql_query("SELECT * FROM Lapor");
+      $tampil = mysql_query("SELECT * FROM Lapor WHERE Verifikasi='Belum Verifikasi'");
       echo
       "<tr>
             <th>No</th>
@@ -75,8 +75,30 @@ include('config.php');
 			?>
 			</table>
   </div>
-  <div class="tab-pane" id="langganan">...
-
+  <div class="tab-pane" id="langganan">
+    <h2>Laporan yang telah diverifikasi</h2>
+    List Tabel Laporan
+      <table class="table table-hover sortable">
+      <?php
+      $tampil = mysql_query("SELECT * FROM Lapor WHERE Verifikasi='Verifikasi'");
+      echo
+      "<tr>
+            <th>No</th>
+            <th>Tanggal</th>
+            <th>Username</th>
+            <th>Isi Laporan</th>
+            <th>Status</th>
+        </tr>";
+       while ($row = mysql_fetch_array($tampil)) {
+        echo "<tr>";
+        echo "<td>" . $row['Id'] . "</td>";
+        echo "<td>" . $row['Tanggal'] . "</td>";
+        echo "<td>" . $row['Name'] . "</td>";
+        echo "<td>" . $row['Isi'] . "</td>";
+        echo "<td>" . $row['Verifikasi'] . "</td>";
+       }
+      ?>
+      </table>
   </div>
   <div class="tab-pane" id="pengaturan">
   	<h2>Pengaturan Twitter</h2>
