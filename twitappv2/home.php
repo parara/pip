0356d20cbe45b11a208fad902b05e11d0831ca1b
tuanpembual <andro.medh4@gmail.com>
2014-11-11 13:30:30 +0700

    add switch

---
+++ twitappv2/home.php
@@ -0,0 +1,27 @@
+<?php $id='home';?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <title>Home | Aplikasi Pelayanan Perizinan Terpadu</title>
+
+<link href="css/bootstrap-theme.css" rel="stylesheet">
+
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/carousel.css" rel="stylesheet">
+<link href="css/docs.min.css" rel="stylesheet">
+<link href="css/xeditable.css" rel="stylesheet">
+<link href="css/table.css" rel="stylesheet">
+
+<link href="css/justified-nav.css" rel="stylesheet">
+
+</head>
+
+<body>
+<h2>Pengaturan Twitter</h2>
+    kode id, hastag
+    bikin form,
+    isi form query, klo udh pake metode insert,
+    butuh validasi isi
+</body>
+</html>
--- twitappv2/index.php
+++ twitappv2/index.php
@@ -1,44 +1,21 @@
 <?php
-include('lock.php');
-include('config.php');
+switch(isset($_GET['id'])) {
+case "home";
+include('home.php');
+break;
 
+case "news":
+include('news.html');
+break;
+}  
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<title>Home | Aplikasi Pelayanan Perizinan Terpadu</title>
-
-<link href="css/bootstrap-theme.css" rel="stylesheet">
-<link href="css/bootstrap.css" rel="stylesheet">
-<link href="css/carousel.css" rel="stylesheet">
-<link href="css/docs.min.css" rel="stylesheet">
-<link href="css/xeditable.css" rel="stylesheet">
-<link href="css/table.css" rel="stylesheet">
-<link href="css/justified-nav.css" rel="stylesheet">
-
+  <title>Judul</title>
+  <a href="index.php?id=home">Home<a/>
+  <a href="news.php?id=news">News<a/>
 </head>
-
 <body>
-<div class="container">
-<?php
-  include('inc/header.php');
-  include('inc/home.php');
-  if (! isset($_GET['page'])){
-    }
-  }
-?>
-
-
-
-</div>
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-  <script src="lib/jquery-1.11.0.js"></script>
-  <script src="lib/bootstrap.js"></script>
-  <script src="lib/docs.min.js"></script>
-  <script src="lib/sorttable.js"></script>  
-<?php include('inc/footer.php');  ?>
 </body>
 </html>
