ce43b5aec7d0e5b44ffc069be75b014bf919ed3b
tuanpembual <andro.medh4@gmail.com>
2014-11-14 22:04:03 +0700

    lets do it

--- twitappv2/index.php
+++ twitappv2/index.php
@@ -1,5 +1,4 @@
 <?php
-echo "sni";
 include('lock.php');
 include('config.php');
 
@@ -27,8 +26,35 @@ switch($_GET['id']) {
 
 }  
 ?>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <title>Home | Aplikasi Pelayanan Perizinan Terpadu</title>
+
+<link href="css/bootstrap-theme.css" rel="stylesheet">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/carousel.css" rel="stylesheet">
+<link href="css/docs.min.css" rel="stylesheet">
+<link href="css/xeditable.css" rel="stylesheet">
+<link href="css/table.css" rel="stylesheet">
+<link href="css/justified-nav.css" rel="stylesheet">
+</head>
+
+<div class="container">
+  <div class="masthead">
+    <h4 class="text-muted">Badan Pelayanan Perizinan Terpadu dan Penanaman Modal Kota Bogor</h4>
+    <ul class="nav nav-justified nav-tabs" role="tablist">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="login.php">Login</a></li>
+    </ul>
+  </div>
 <body>
-  sni
-</body>
-</html>
+  <h2>Ini disi apa</h2>
+  <p class="text-danger">As of v7.0.1, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
+  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p> 
+</div>
+
+<?php
+include('inc/footer.php');
+?>
