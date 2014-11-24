f30026c6d3101182c8e42c16123527d464ea0a67
tuanpembual <andro.medh4@gmail.com>
2014-11-24 18:23:43 +0700

    perbaikan cron

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python /home/nona/pip/mining.py > /home/nona/pip/mining.json #use move old twit
-python /home/nona/pip/save.py
+python mining.py > mining.json #use move old twit
+python save.py
