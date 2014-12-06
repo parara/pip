570c17db02b4ec6c6218a0e4fff2fdd59e82dc5e
tuanpembual <andro.medh4@gmail.com>
2014-12-06 15:37:55 +0700

    next, tampilan

--- twitappv2/process.php
+++ twitappv2/process.php
@@ -3,20 +3,23 @@ include("inc/config.php");
 
 $user_check="admin";
 // add jam edit;
-if($_GET['id'] and $_GET['data'])
+if(empty($_GET['id']) or empty($_GET['data']))
 {
+  return false;
+}
+
   $id = $_GET['id'];
   $data = $_GET['data'];
+    //how id
+  if (strlen($data) == 1) {
+    $kolom = "id_progres"; 
+  } else {
+    $kolom = "komentar";
+  }
+  $sql = "UPDATE lapor SET $kolom='$data', editor='$user_check' WHERE id = $id";
+  //print_r($sql);exit;
+  if(mysql_query($sql))
+    echo 'success';
 
-  // if (strlen($data)==1)
-  //   $isian = "id_progres";
-  // else {
-  //   $isian = "komentar";
-  // }
-  
-  //how id
-  if(mysql_query("UPDATE lapor SET '$isian'='$data', editor='$user_check' WHERE id='$id'"))
-  echo 'success';
-}
 
 ?>
